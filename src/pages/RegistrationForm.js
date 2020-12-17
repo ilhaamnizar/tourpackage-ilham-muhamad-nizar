6520faf94c8cd35415b01abbbb8668f032aac63f
ilhaamnizar <ilhamnzrr21@gmail.com>
2020-12-18 04:16:04 +0700

    first summit

---
+++ src/pages/RegistrationForm.js
@@ -0,0 +1,76 @@
+import { TextField, Grid, makeStyles } from '@material-ui/core';
+import React, { useState, useEffect } from 'react'
+
+const useStyle = makeStyles(theme => ({
+    root:{
+        '& .MuiFormControl-root': {
+            width: '80%',
+            margin: theme.spacing(1)
+        }
+    }
+}))
+
+const initialFValues = {
+    id:0,
+    name: '',
+    price:'',
+    address: '',
+    description: '',
+}
+
+function RegistrationForm() {
+
+    const [values, setValues] = useState(initialFValues);
+    const classes = useStyle();
+
+    const handleInputChange = event => {
+        const {name, value} = event.target
+        setValues({
+            ...values,
+            [name]: value
+        })
+    }
+
+    return (
+        <form className={classes.root}>
+            <Grid container>
+                <Grid item xs={6}>
+                    <TextField 
+                    variant="outlined"
+                    label="Nama Tempat"
+                    name="namatempat"
+                    value={values.name}
+                    onChange ={handleInputChange}
+                    />
+                    <TextField 
+                    variant="outlined"
+                    label="Alamat"
+                    name="alamat"
+                    value={values.address}
+                    onChange={handleInputChange}
+                    />
+                </Grid>
+                <Grid item xs={6}>
+                <TextField 
+                    variant="outlined"
+                    label="Harga"
+                    name="harga"
+                    value={values.price}
+                    onChange={handleInputChange}
+                    />
+                    <TextField 
+                    variant="outlined"
+                    label="Description"
+                    multiline
+                    rows={4}
+                    name="description"
+                    value={values.description}
+                    onChange={handleInputChange}
+                    />
+                </Grid>
+            </Grid>
+        </form>
+    )
+}
+
+export default RegistrationForm
